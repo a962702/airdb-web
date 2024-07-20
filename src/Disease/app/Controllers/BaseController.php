daf2b9ae0e9bbb49c8a924ac2079080fcd823a99
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-21 00:02:34 +0800

    BaseController: Add postGetAllName()

--- src/Disease/app/Controllers/BaseController.php
+++ src/Disease/app/Controllers/BaseController.php
@@ -64,4 +64,28 @@ abstract class BaseController extends Controller
         $session = session();
         $session->remove('user');
     }
+
+    /**
+     * GetAllName
+     */
+    public function postGetAllName()
+    {
+        $result_array = array(
+            "HBV",
+            "HCV",
+            "Cancer",
+            "Dementia",
+            "Hyperlipidemia",
+            "Diabetes Mellitus",
+            "Myocardial Infraction",
+            "Chronic Kidney Disease",
+            "Cerebrovascular Disease",
+            "Congestive Heart Failure",
+            "Peripheral Vascular Disease",
+            "Pneumonia",
+            "Obstructive Lung Disease",
+            "Acute Renal Failure",
+        );
+        echo json_encode($result_array);
+    }
 }
