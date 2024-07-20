32877162a571c412839c5f2ccf47d0702742161c
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-21 00:41:34 +0800

    Login: change

--- src/Disease/app/Controllers/Login.php
+++ src/Disease/app/Controllers/Login.php
@@ -11,41 +11,41 @@ class Login extends BaseController
 
     public function postcheckLogin()
     {
-		if(isset($_POST['account']) && isset($_POST['password']))
-		{
-			$u_account = $this->request->GetPost('account');
-			$u_password = $this->request->GetPost('password');
-			$command = '/usr/bin/python3 ./DiseaseFile/Login.py'." ".$u_account." ".$u_password;
-			//exec($command,$result_array);
-			$result_array = ["2"];
-			if(!strcmp($result_array[0],"2")
-			){
-				echo json_encode($result_array);
-				exit(0);
-			}
-			
-			if($result_array[0]==="2")
-			{
-				echo json_encode($result_array);
-			}
-			else
-			{
-				echo json_encode($result_array);
-			}
-		}
-	}
-	function postsetSession()
+        if(isset($_POST['account']) && isset($_POST['password']))
+        {
+            $u_account = $this->request->GetPost('account');
+            $u_password = $this->request->GetPost('password');
+            $command = '/usr/bin/python3 ./DiseaseFile/Login.py'." ".$u_account." ".$u_password;
+            //exec($command,$result_array);
+            $result_array = ["2"];
+            if(!strcmp($result_array[0],"2")
+            ){
+                echo json_encode($result_array);
+                exit(0);
+            }
+            
+            if($result_array[0]==="2")
+            {
+                echo json_encode($result_array);
+            }
+            else
+            {
+                echo json_encode($result_array);
+            }
+        }
+    }
+    function postsetSession()
     {
-		$session = session();
+        $session = session();
         $u_account = $this->request->GetPost('account');
-		$session->set('user',$u_account);
-		echo json_encode($u_account);
-	}
+        $session->set('user',$u_account);
+        echo json_encode($u_account);
+    }
 
 
-	function postgetSession()
+    function postgetSession()
     {
-		$session = session();
+        $session = session();
         $session->remove('user');
-	}
+    }
 }
