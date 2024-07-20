a052a63bbd4ed8d5fc9a258b06a860a3a792a07f
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-20 22:04:43 +0800

    Add Login

---
+++ src/Disease/app/Controllers/Login.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controllers;
+
+class Login extends BaseController
+{
+    public function getIndex(): string
+    {
+        return view('Login');
+    }
+
+    public function postcheckLogin()
+    {
+		if(isset($_POST['account']) && isset($_POST['password'])){
+			$u_account = $this->input->post('account',true);
+			$u_password = $this->input->post('password',true);
+			$command = '/usr/bin/python3 ./DiseaseFile/Login.py'." ".$u_account." ".$u_password;
+			//exec($command,$result_array);
+			$result_array = ["2"];
+			if(!strcmp($result_array[0],"2")){
+				echo json_encode($result_array);
+				exit(0);
+			}
+			
+			if($result_array[0]==="2"){
+				echo json_encode($result_array);
+			}else{
+				echo json_encode($result_array);
+			}
+		}
+	}
+	function postsetSession()
+    {
+		$u_account = $this->request->GetPost('account');
+		$this->session->set('user',$u_account);
+		echo json_encode($u_account);
+	}
+
+
+	function postgetSession()
+    {
+		$this->session->remove('user');
+	}
+}
