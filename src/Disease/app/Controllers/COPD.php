8f7a189259afd935b2023af49862e6933178df17
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-21 00:04:21 +0800

    COPD: change

--- src/Disease/app/Controllers/COPD.php
+++ src/Disease/app/Controllers/COPD.php
@@ -8,29 +8,9 @@ class COPD extends BaseController
     {
         return view('COPD');
     }
-    
-    function postGetAllName(){
-        $result_array = array(
-            "HBV",
-            "HCV",
-            "Cancer",
-            "Dementia",
-            "Hyperlipidemia",
-            "Diabetes Mellitus",
-            "Myocardial Infraction",
-            "Chronic Kidney Disease",
-            "Cerebrovascular Disease",
-            "Congestive Heart Failure",
-            "Peripheral Vascular Disease",
-            "Pneumonia",
-            "Obstructive Lung Disease",
-            "Acute Renal Failure",
-        );
-        echo json_encode($result_array);
-    }
 
-    function postGetResult() {
-
+    public function postGetResult()
+    {
         $url = 'http://model_service:8000/model/COPD';
         $ml_data=$this->request->GetPost('ml_data');
         
@@ -41,7 +21,8 @@ class COPD extends BaseController
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch,CURLOPT_HTTPHEADER,$headerArray);
         $result = curl_exec($ch);
-        if (!$result) {
+        if (!$result)
+        {
             die("failure connect");
         }
         curl_close($ch);
