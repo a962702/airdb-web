8a78b167c4b41d79125369f5dd4f5b2d02b9849b
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-20 23:43:28 +0800

    AirMA: fix Logout

--- src/Disease/app/Views/AirMA.php
+++ src/Disease/app/Views/AirMA.php
@@ -404,7 +404,7 @@
 
 	function out() {
 		$.ajax({
-			url: "<?php echo base_url('AirMA/out')?>",
+			url: "<?php echo base_url('AirMA/Logout')?>",
 			type: "post",
 			success: function (data) {
 				window.location.href = 'Login';
