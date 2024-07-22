96cd21311e4b4dcdbd10d02d515034b267428cc0
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-22 16:23:57 +0800

    COPD: add postFetchAQI()

--- src/Disease/app/Controllers/COPD.php
+++ src/Disease/app/Controllers/COPD.php
@@ -9,6 +9,24 @@ class COPD extends BaseController
         return view('COPD');
     }
 
+    function postFetchAQI()
+    {
+        $url = 'http://aqi:8000/aqi/get';
+        $addr=$this->request->GetPost('addr');
+        $date=$this->request->GetPost('date');
+        $period=$this->request->GetPost('period');
+        $url = $url . '?addr=' . urlencode($addr) . '&date=' . urlencode($date) . '&period=' . urlencode($period);
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        
+        $output = curl_exec($ch);
+        curl_close($ch);
+        
+        echo $output;
+    }
+
     public function postGetResult()
     {
         $url = 'http://model_COPD:8000/predict';
