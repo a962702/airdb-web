b9b2027c8f0ffb9d93e18d3759f46599643b0adf
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-22 16:01:02 +0800

    AirBox: fix

--- src/Disease/app/Views/AirBox.php
+++ src/Disease/app/Views/AirBox.php
@@ -166,7 +166,7 @@
                                 "<div class='card m-b-30'> <div class='card-body'> <h8>" + res + "</h8> </div> </div>";
                             
                             document.getElementById("imageShow").innerHTML =
-                                "<img src='<?php echo base_url('airBox/getFigOne')?>?ver=" + timestamp + "'  width='100%'/> <br> <br> <img src='<?php echo base_url('airBox/getFigOne')?>?ver=" + timestamp + "'  width='100%'/>";
+                                "<img src='<?php echo base_url('airBox/getFigOne')?>?ver=" + timestamp + "'  width='100%'/> <br> <br> <img src='<?php echo base_url('airBox/getFigTwo')?>?ver=" + timestamp + "'  width='100%'/>";
                 }
                         /*,
                         error: function (data) {
