1c9a756f945f588ca8daf6f1ff2a8b8587fad309
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-22 15:12:50 +0800

    AirBox: update

--- src/Disease/app/Controllers/AirBox.php
+++ src/Disease/app/Controllers/AirBox.php
@@ -9,24 +9,47 @@ class AirBox extends BaseController
         return view('AirBox');
     }
 
-    public function ml_Model(){
-		$address = $this->input->post('address');		
-		$command = '/opt/miniconda3/bin/python3 ./DiseaseFile/airBox.py'." ".$address." ".'87';
-		exec($command,$result_array);
-		echo json_encode($result_array);
-	}
-
     public function postGetResult()
     {
-        $url = 'http://model_service:8000/model/AirBox';
-        $ml_data=$this->request->GetPost('ml_data');
+        $url = 'http://model_airBox:8000/run';
+        $address=$this->request->GetPost('address');
         
         $headerArray=array("Content-type:application/json;","Accept:application/json");
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $ml_data);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, array("address" => $address));
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         curl_setopt($ch,CURLOPT_HTTPHEADER,$headerArray);
+        $result = curl_exec($ch);
+        if (!$result)
+        {
+            die("failure connect");
+        }
+        curl_close($ch);
+        echo $result;
+    }
+
+    public function getGetFigOne()
+    {
+        $url = 'http://model_airBox:8000/fig_one';
+        
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $result = curl_exec($ch);
+        if (!$result)
+        {
+            die("failure connect");
+        }
+        curl_close($ch);
+        echo $result;
+    }
+
+    public function getGetFigTwo()
+    {
+        $url = 'http://model_airBox:8000/fig_two';
+        
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
         $result = curl_exec($ch);
         if (!$result)
         {
