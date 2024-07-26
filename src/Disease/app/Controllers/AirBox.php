8fbd3742604d2428999f75fca755fec977ae82c1
Andy Yang <23339548+a962702@users.noreply.github.com>
2024-07-26 14:46:45 +0800

    AirBox: update model api url

--- src/Disease/app/Controllers/AirBox.php
+++ src/Disease/app/Controllers/AirBox.php
@@ -18,7 +18,7 @@ class AirBox extends BaseController
 
     public function postGetResult()
     {
-        $url = 'http://model_airBox:8000/run';
+        $url = 'http://airdb_model_airBox:8000/run';
         $address=$this->request->GetPost('address');
         
         $headerArray=array("Content-type:application/json;","Accept:application/json");
@@ -38,7 +38,7 @@ class AirBox extends BaseController
 
     public function getGetFigOne()
     {
-        $url = 'http://model_airBox:8000/fig_one';
+        $url = 'http://airdb_model_airBox:8000/fig_one';
         
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -53,7 +53,7 @@ class AirBox extends BaseController
 
     public function getGetFigTwo()
     {
-        $url = 'http://model_airBox:8000/fig_two';
+        $url = 'http://airdb_model_airBox:8000/fig_two';
         
         $ch = curl_init($url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
